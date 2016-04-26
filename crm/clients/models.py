803f58e33f08a17da5ccbe25d4a9a2c2f210f46a
dyugay <dyugay@yandex.ru>
2016-04-26 12:41:38 +0600

    added tables for client account and legal details

--- crm/clients/models.py
+++ crm/clients/models.py
@@ -14,9 +14,9 @@ class Client(models.Model):
 
 class clientContactDetails(models.Model):
   clientId = models.ForeignKey(Client)
-  telephoneNum1 = models.IntegerField()
-  telephoneNum2 = models.IntegerField()
-  telephoneNum3 = models.IntegerField()
+  telephoneNum1 = models.CharField(max_length = 11)
+  telephoneNum2 = models.CharField(max_length = 11)
+  telephoneNum3 = models.CharField(max_length = 11)
   email1 = models.EmailField()
   email2 = models.EmailField()
   firstName = models.CharField(max_length = 20)
@@ -24,5 +24,27 @@ class clientContactDetails(models.Model):
   midleName = models.CharField(max_length = 40)
   author = models.ForeignKey(User)
   addedAt = models.DateTimeField(auto_now_add = True)
+
+class legal_details(models.Model):
+  cleintId = models.ForeignKey(Client)
+  personal_ID = models.CharField(max_length=12)
+  city = models.CharField(max_length = 20)
+  company_name = models.CharField(max_length = 40)
+  BIN =  models.CharField(max_length = 12)
+  bank_account = models.CharField(max_length = 25)
+  bank = models.CharField(max_length = 20)
+  author = models.ForeignKey(User)
+  addedAt = models.DateTimeField(auto_now_add = True)
+
+class account(models.Model):
+  account = models.EmailField()
+  clientId = models.ForeignKey(Client)
+  account_added_at = models.DateTimeField()
+  inactive = models.CharField(max_length = 1)
+  author = models.ForeignKey(User)
+  addedAt = models.DateTimeField(auto_now_add = True)
+
+
+
 
 
