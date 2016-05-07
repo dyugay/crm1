41d7df1629b02318c19ad7a24f080f6638686e66
dyugay <dyugay@yandex.ru>
2016-05-07 21:00:21 +0600

    added address fiels into models.py

--- crm/clients/models.py
+++ crm/clients/models.py
@@ -29,6 +29,7 @@ class Legal_details(models.Model):
   cleintId = models.ForeignKey(Client)
   personal_ID = models.CharField(max_length=12)
   city = models.CharField(max_length = 20)
+  address = models.CharField(max_length = 40)
   company_name = models.CharField(max_length = 40)
   BIN =  models.CharField(max_length = 12)
   bank_account = models.CharField(max_length = 25)
